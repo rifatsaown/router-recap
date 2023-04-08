f2976afa0e3544e7b97f5b0bd86b5cdb862c9355
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-04-08 21:27:53 +0600

    React context implement

--- src/Components/Cousin.jsx
+++ src/Components/Cousin.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { RingContext } from './Granpa';
+import { MoneyContext, RingContext } from './Granpa';
 
 const Cousin = ({children}) => {
     const ring = React.useContext(RingContext);
+    const [money] = React.useContext(MoneyContext);
     return (
         <div className='border-4 border-green-800 p-2 mr-2'>
             <p>{children}</p>
             <p>{ring}</p>
+            <p>{money}</p>
         </div>
     );
 };
